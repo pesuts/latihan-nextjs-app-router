266b968287a5053dd5f1ee25304430ff726c0250
pesuts <daniel.sinaga.ds@gmail.com>
2024-10-20 04:10:24 +0700

    feat: implement Data Fetching

--- src/app/api/product/route.ts
+++ src/app/api/product/route.ts
@@ -4,24 +4,42 @@ export async function GET(request: NextRequest) {
   const { searchParams } = new URL(request.url);
   const id = searchParams.get("id");
   // const id = new URL(request.url).searchParams.get("id");
-  // console.log(id);
-  
+
   const data = [
     {
       id: 1,
-      name: "Sepatu Baru",
-      price: 100000,
+      title: "Nike Invicible 3",
+      price: 3890000,
+      image:
+        "https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,q_auto:eco/05101027-9c5a-4b75-b257-2e9f0627acf3/NIKE+ZOOMX+INVINCIBLE+RUN+FK+3.png",
     },
     {
       id: 2,
-      name: "Sepatu Lama",
-      price: 35000,
+      title: "Nike V2K Run",
+      price: 1690000,
+      image:
+        "https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,q_auto:eco/4f323c12-a7a6-4ff3-bea2-50c26ca2ca86/W+NIKE+V2K+RUN.png",
+    },
+    {
+      id: 3,
+      title: "Nike Full Force Low",
+      price: 1590000,
+      image:
+        "https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,q_auto:eco/8f08b820-f450-4a87-b689-c7091cc88a9f/NIKE+FULL+FORCE+LO.png",
     },
   ];
   console.log(data.find((data) => data.id === Number(id)));
-  const detailData = data.find((data) => data.id === Number(id));
-  if (!detailData) { 
-    return NextResponse.json({ status: 404, message: "Not Found", data: {} });
+  // let detailData = undefined;
+  if (id) {
+    const detailData = data.find((data) => data.id === Number(id));
+    if (!detailData) {
+      return NextResponse.json({ status: 404, message: "anjing" });
+    }
+    return NextResponse.json({
+      status: 200,
+      message: "Success",
+      data: detailData,
+    });
   }
-  return NextResponse.json({ status: 200, message: "Success", data: detailData });
+  return NextResponse.json({ status: 200, message: "Success", data });
 }
--- src/app/product/[[...slug]]/page.tsx
+++ src/app/product/[[...slug]]/page.tsx
@@ -1,14 +1,118 @@
+import { randomInt } from "crypto";
+import Image from "next/image";
+import Link from "next/link";
+import { FaCartShopping, FaFire } from "react-icons/fa6";
+
 type DetailProductPageProps = {
   params: { slug: string };
 };
 
-export default function DetailProductPage(props: DetailProductPageProps) {
+async function getData() {
+  const response = await fetch("https://fakestoreapi.com/products");
+  // const response = await fetch("http://localhost:3000/api/product", {
+  //   cache: "no-store",
+  // });
+  if (!response.ok) {
+    throw new Error("Failed to fetch data");
+  }
+  // console.log(response);
+  return response.json();
+}
+
+export default async function DetailProductPage(props: DetailProductPageProps) {
   const { params } = props;
-  console.log(params);
-  console.log("wew");
+  const products = await getData();
+  const productsData = products?.data ?? products;
   return (
     <div>
       <h1> {params.slug ? "Detail Product Page" : "Product Page"}</h1>
+      {productsData.length > 0 && (
+        <div className="grid grid-cols-4 gap-8 m-5">
+          {productsData.map(
+            (product: {
+              id: number;
+              title: string;
+              price: number;
+              description: string;
+              category: string;
+              image: string;
+              rating: string;
+            }) => {
+              const discount = 5 + randomInt(25);
+              return (
+                <Link
+                  href={`/product/${product.id}`}
+                  key={product.id}
+                  className="border-2 rounded-md duration-500 bg-white hover:z-50 relative hover:scale-110"
+                >
+                  {discount > 20 ? (
+                    <div className="absolute top-6 left-0 w-14 h-6 z-50 bg-red-600">
+                      <div className="flex items-center gap-1">
+                        <FaFire size={16} className="text-white ps-1" />
+                        <p className="text-white text-center">Hot!</p>
+                      </div>
+                    </div>
+                  ) : (
+                    ""
+                  )}
+                  <div className="flex flex-col justify-between h-full">
+                    <div className="px-8 py-4">
+                      {product.image && (
+                        <Image
+                          src={product.image}
+                          className="w-full h-60 object-contain my-2"
+                          alt={product.image}
+                          width={300}
+                          height={300}
+                        />
+                      )}
+                      <p className="text-md font-bold">{product.title}</p>
+                    </div>
+                    {/* <div className="grow"></div> */}
+                    <div className="border-t-4 bg-slate-100">
+                      <div className="px-6 py-4">
+                        <div className="flex justify-between items-center">
+                          <div className="items-start content-start justify-start">
+                            <div className="flex items-center gap-1">
+                              <p className="text-slate-500 text-sm line-through">
+                                {(
+                                  (discount / 100) * product.price +
+                                  product.price
+                                ).toLocaleString("en", {
+                                  currency: "USD",
+                                  style: "currency",
+                                })}
+                              </p>
+                              <span className="text-red-500 bg-red-100 text-xs rounded-sm px-1">
+                                -{Math.floor(discount)}%
+                              </span>
+                            </div>
+                            <p className="text-red-600 font-bold text-xl">
+                              {product.price.toLocaleString("en", {
+                                currency: "USD",
+                                style: "currency",
+                              })}
+                            </p>
+                          </div>
+                          <Link
+                            href={`/product/add/${product.id}`}
+                            className="bg-red-600 text-white hover:bg-red-800 rounded-md px-2 py-3"
+                          >
+                            <div className="flex items-center gap-2 justify-center">
+                              <FaCartShopping size={18} />
+                              <p className="text-sm">Add to Cart</p>
+                            </div>
+                          </Link>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </Link>
+              );
+            }
+          )}
+        </div>
+      )}
       {params.slug && (
         <>
           {params?.slug[0] && <p> Category: {params.slug[0]}</p>}
