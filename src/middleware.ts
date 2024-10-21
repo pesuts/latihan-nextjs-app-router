f5440b52727bbb5d73bcc9e7090bb9a69eb52f6e
pesuts <daniel.sinaga.ds@gmail.com>
2024-10-21 14:03:03 +0700

    feat: implement Middleware

---
+++ src/middleware.ts
@@ -0,0 +1,19 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+export function middleware(request: NextRequest) {
+  // console.log(request.url);
+  // console.log("cuki");
+  const isLogin = false;
+
+  // if (request.nextUrl.pathname.startsWith("/about")) {
+  if (!isLogin) {
+    return NextResponse.redirect(new URL("/login", request.url));
+  }
+  // }
+  // return NextResponse.next();
+}
+
+export const config = {
+  matcher: ["/dashboard/:path*", "/about/:path*"]
+};
